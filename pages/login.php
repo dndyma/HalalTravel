2297f3f0c31cf09eb7434333ec9931cc31f05c47
dndyma <dandyanugerah004@gmail.com>
2025-05-11 15:42:25 +0700

    modify : login.css

--- pages/login.php
+++ pages/login.php
@@ -29,73 +29,12 @@ if (isset($_POST['login'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
-    <style>
-        * {
-            padding: 0;
-            margin: 0;
-            box-sizing: border-box;
-            font-family: sans-serif;
-        }
-
-        body,
-        html {
-            height: 100%;
-            width: 100%;
-        }
-
-        .container {
-            position: relative;
-            width: 100%;
-            height: 100vh;
-            display: flex;
-            justify-content: flex-end;
-            align-items: center;
-        }
-
-        .img-login {
-            position: absolute;
-            z-index: -1;
-            width: 100%;
-            height: 100%;
-            object-fit: cover;
-        }
-
-        .form-login {
-            background-color: rgba(255, 255, 255, 1);
-            /* semi transparan */
-            padding: 30px;
-            border-radius: 30px;
-            margin-right: 80px;
-            width: 500px;
-            text-align: center;
-            height: 500px
-        }
-
-        .form-login input[type="text"],
-        .form-login input[type="password"] {
-            width: 100%;
-            padding: 10px;
-            margin: 10px 0;
-            border-radius: 5px;
-            border: 1px solid #ccc;
-        }
-
-        .form-login input[type="submit"] {
-            width: 100%;
-            padding: 10px;
-            background-color: navy;
-            color: white;
-            border: none;
-            border-radius: 5px;
-            margin-top: 10px;
-            cursor: pointer;
-        }
-    </style>
+    <link rel="stylesheet" href="../assets/css/root.css">
+    <link rel="stylesheet" href="../assets/css/login.css">
 </head>
 
 <body>
     <div class="container">
-        <img src="../assets/img/tajmahal.jpg" class="img-login" alt="Background">
         <form action="login.php" class="form-login" method='POST'>
             <p>Login To Continue</p>
             <input type="text" placeholder="Username" name='username'>
