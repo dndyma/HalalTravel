79a21ddfd0362ed25cb106efdd581b439c3531c7
agung-anugrah <agunganugrahiillahi123@gmail.com>
2025-05-12 00:17:56 +0700

    tambah-admin-uniqid

--- feature/register.php
+++ feature/register.php
@@ -1,7 +1,7 @@
 <?php
 include '../db/koneksi.php';
 session_start();
-$id=0;
+$id_user =  uniqid();
 $register_in='';
 if (isset($_POST['register'])) {
     $username = $_POST['username'];
@@ -13,14 +13,9 @@ if (isset($_POST['register'])) {
     else{
         $sql = "SELECT id_user FROM user";
         $result = $db->query($sql);
-        while ($row = $result->fetch_assoc()) {
-            $id+=1;
-        }
-        $id+=1;
-        $id_user = 'user_'.$id;
         $sql = "INSERT into user value('$id_user','$username','$password','$role')";
         $result = $db->query($sql);
-        header('location:index.php');
+        header('location:../pages/index.php');
     }
 }
 ?>
