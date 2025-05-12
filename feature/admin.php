9cb7eed00ad97e2ac893a535660fe7f0787ea149
agung-anugrah <agunganugrahiillahi123@gmail.com>
2025-05-13 03:32:03 +0700

    view detai

--- feature/admin.php
+++ feature/admin.php
@@ -9,13 +9,8 @@ $harga = $_POST['harga'];
 $lokasi = $_POST['lokasi'];
 $id = uniqid();
 $rating = $_POST['rating'];
-<<<<<<< HEAD
 if(!is_numeric($harga) || empty($tittle) || empty($gambar) || empty($harga) || empty($lokasi)){
     $destinasi_in='input harga harus berupa angka';
-=======
-if (!is_numeric($harga)) {
-    $destinasi_in = 'input harga harus berupa angka';
->>>>>>> de4cada3ce8e21cba4a579deab7fe648cd98c276
     header('location: ../pages/admin.php');
 } else {
     $sql = "INSERT into  destinasi values('$id','$tittle','$gambar','$lokasi','$harga','$rating')";
